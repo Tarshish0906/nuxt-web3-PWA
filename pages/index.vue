38b72b089a52631d1ee656343db65fb41207ec2a
Tarshish0906 <k.oxygen08@gmail.com>
2021-03-28 17:02:09 +0900

    console.logでチェーンと繋がっているか確認しました

--- pages/index.vue
+++ pages/index.vue
@@ -2,33 +2,33 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">
-        nuxt-web3
-      </h1>
+      <h1 class="title">nuxt-web3</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
           rel="noopener noreferrer"
           class="button--green"
-        >
-          Documentation
-        </a>
+        >Documentation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           rel="noopener noreferrer"
           class="button--grey"
-        >
-          GitHub
-        </a>
+        >GitHub</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    console.log("Current Block Number");
+    // その時点でのBlockHeight、つまり承認済みの最新のブロックの番号を返してくれるメソッド
+    this.$web3.eth.getBlockNumber().then(console.log);
+  }
+};
 </script>
 
 <style>
@@ -42,16 +42,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
