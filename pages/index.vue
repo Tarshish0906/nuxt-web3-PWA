1358545ccee96976adb0f9572229f55837c20b2a
Tarshish0906 <k.oxygen08@gmail.com>
2021-03-28 19:50:42 +0900

    スマートコントラクトとの通信を行う

--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,48 @@
       <Logo />
       <h1 class="title">nuxt-web3</h1>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >GitHub</a>
+        <input type="text" v-model="inputNumber" placeholder="input number" />
+        <button @click="setNumber()">
+          Set Number to contract
+        </button>
       </div>
+      <div class="links">
+        <button @click="getNumber()">
+          Get Number from contract
+        </button>
+      </div>
+      <div>Number:{{ number }}</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // コントラクトから取得する数値
+      number: 0,
+      // フォームから入力された数値
+      inputNumber: 0
+    };
+  },
+  methods: {
+    getNumber: async function() {
+      // コントラクトからの読み込み部分
+      let ret = await this.$contract.methods.get().call();
+      // フロントへ反映
+      this.number = ret;
+    },
+    setNumber: async function() {
+      // MetaMaskで使っているアカウントの取得
+      let accounts = await this.$web3.eth.getAccounts();
+      let account = accounts[0];
+      // コントラクトへの書き込み部分
+      let ret = await this.$contract.methods
+        .set(this.inputNumber)
+        .send({ from: account });
+    }
+  },
   mounted() {
     console.log("Current Block Number");
     // その時点でのBlockHeight、つまり承認済みの最新のブロックの番号を返してくれるメソッド
--- plugins/web3.js
+++ plugins/web3.js
@@ -1,4 +1,6 @@
 import Web3 from "web3";
+// コントラクトのコンパイル後の設定ファイルの読み込み
+import artifacts from "~~/build/contracts/SingleNumRegister.json";
 
 export default async function(context, inject) {
   let web3;
@@ -17,7 +19,19 @@ export default async function(context, inject) {
   } else {
     // httpEndpoint で接続するブロックチェーンの情報を設定する
     const httpEndpoint = "http://127.0.0.1:7545";
-    web3 = new Web3(new web3.providers.HttpProvider(httpEndpoint));
+    web3 = new Web3(new Web3.providers.HttpProvider(httpEndpoint));
   }
+  // チェーンのネットワークIDを取得
+  let networkId = await web3.eth.net.getId();
+  // コントラクトのインスタンスの初期化
+  // 設定ファイルとアドレスが必要
+  let contract = new web3.eth.Contract(
+    // コントラクトのインスタンス後の設定ファイル
+    artifacts.abi,
+    // ネットワークID毎に保存されているコントラクトのアドレスを読みこむ
+    artifacts.networks[networkId].address
+  );
   inject("web3", web3);
+  // インスタンスを生やす
+  inject("contract", contract);
 }
