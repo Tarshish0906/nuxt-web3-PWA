09f94fbc25ee40f1e1cd4ba87d537302f766b434
Tarshish0906 <k.oxygen08@gmail.com>
2021-04-01 19:40:57 +0900

    githubのhdwallet-providerの通りにしましたが、Error: Could not create addresses from your mnemonic or private key(s).というエラーがでます

--- truffle-config.js
+++ truffle-config.js
@@ -23,6 +23,7 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 //
 // const fs = require("fs");
 // const mnemonic = fs.readFileSync(".envrc").toString().trim();
+const mnemonicPhrase = process.env.MNEMONIC;
 
 module.exports = {
   /**
@@ -59,52 +60,57 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-      provider: () =>
-        new HDWalletProvider(
-          process.env.MNEMONIC,
-          `https://ropsten.infura.io/v3/` + process.env.INFURA_PROJECT_ID
-        ),
-      network_id: 3, // Ropsten's id
-      gas: 5500000 // Ropsten has a lower block limit than mainnet
-      // confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
-    }
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
-  },
+      provider: () => {
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: mnemonicPhrase
+          },
+          providerOrUrl:
+            `https://ropsten.infura.io/v3/` + process.env.INFURA_PROJECT_ID
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
-  compilers: {
-    solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
+          // gas: 5500000 // Ropsten has a lower block limit than mainnet
+          // confirmations: 2, // # of confs to wait between deployments. (default: 0)
+          // timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+          // skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+        });
+      },
+      network_id: "3" // Ropsten's id
+      // Useful for private networks
+      // private: {
+      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+      // network_id: 2111,   // This network is yours, in the cloud.
+      // production: true    // Treats this network as if it was a public net. (default: false)
       // }
+    },
+
+    // Set default mocha options here, use special reporters etc.
+    mocha: {
+      // timeout: 100000
+    },
+
+    // Configure your compilers
+    compilers: {
+      solc: {
+        // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+        // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+        // settings: {          // See the solidity docs for advice about optimization and evmVersion
+        //  optimizer: {
+        //    enabled: false,
+        //    runs: 200
+        //  },
+        //  evmVersion: "byzantium"
+        // }
+      }
+    },
+
+    // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
+    //
+    // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+    // those previously migrated contracts available in the .db directory, you will need to run the following:
+    // $ truffle migrate --reset --compile-all
+
+    db: {
+      enabled: false
     }
-  },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
-  //
-  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
-  // those previously migrated contracts available in the .db directory, you will need to run the following:
-  // $ truffle migrate --reset --compile-all
-
-  db: {
-    enabled: false
   }
 };
